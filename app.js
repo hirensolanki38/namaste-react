cccfbed188204e8cbcda67718d9642e1a55c801e
Hiren Solanki <hiren.solanki@borngroup.com>
2026-01-06 13:48:14 +0530

    new changes of files

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React !!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello World from React by Hiren Solanki !!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
